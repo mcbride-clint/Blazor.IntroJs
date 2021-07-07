53f020911692b5e67e06b816227291fc1a6ab2e2
mcbride-clint <cmm5559@gmail.com>
2021-07-07 17:00:45 -0400

    Restructure Solution using dotnet-boxed

--- samples/Blazor.IntroJs.Wasm/Pages/QuickStart.razor.cs
+++ samples/Blazor.IntroJs.Wasm/Pages/QuickStart.razor.cs
@@ -1,75 +1,20 @@
-﻿using Microsoft.AspNetCore.Components;
-using System;
-using System.Threading.Tasks;
-
 namespace Blazor.IntroJs.Wasm.Pages
 {
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Components;
+
     public partial class QuickStart : ComponentBase, IAsyncDisposable
     {
         [Inject] private IntroJsInterop IntroJs { get; set; }
-
 
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
             if (firstRender)
             {
                 await IntroJs
-                    .OnHintClick(OnHint)
-                    .OnBeforeChange(OnBeforeChange)
-                    .OnAfterChange(OnAfterChange)
-                    .OnChange(OnChange)
-                    .OnComplete(OnComplete)
-                    .OnExit(OnExit)
-                    .OnHintClose(OnHintClose)
-                    .OnHintsAdded(OnHintsAdded)
                     .Start();
             }
-        }
-
-        private void OnHintsAdded(IntroJsArgs args)
-        {
-            var x = 0;
-        }
-
-        private void OnHintClose(IntroJsArgs args)
-        {
-            var x = 0;
-        }
-
-        private void OnExit(IntroJsArgs args)
-        {
-            var x = 0;
-        }
-
-        private void OnComplete(IntroJsArgs args)
-        {
-            var x = 0;
-        }
-
-        private void OnChange(object obj, IntroJsArgs args)
-        {
-            var x = 0;
-        }
-
-        private void OnAfterChange(object obj, IntroJsArgs args)
-        {
-            var x = 0;
-        }
-
-        private bool OnBeforeChange(object obj, IntroJsArgs args)
-        {
-            
-            return args.CurrentStep == 0;
-        }
-
-        private void OnHint(IntroJsArgs args)
-        {
-            var x = 0;
-        }
-
-        protected bool OnBeforeExit(IntroJsArgs args)
-        {
-            return true;
         }
 
         protected async Task StartIntroJs()
